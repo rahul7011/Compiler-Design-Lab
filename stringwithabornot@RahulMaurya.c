6741bbf420469b154ea2b76b7278be22d0f5129c
Rahul maurya <champrahul01@gmail.com>
2021-09-23 10:40:42 +0530

    Update and rename stringwithabornot.c to stringwithabornot@RahulMaurya.c

--- stringwithabornot@RahulMaurya.c
+++ stringwithabornot@RahulMaurya.c
@@ -3,8 +3,8 @@
 
 void main() {
     char ch[100];
-    printf("Name:Sachin Tiwari \n");
-    printf("Roll No.:1901330100236 \n");
+    printf("Name:Rahul Maurya \n");
+    printf("Roll No.:1901330100210 \n");
     printf("Branch/Section: C.S.E. ,5 C \n");
     printf("Enter string-");
     scanf("%s", &ch);
