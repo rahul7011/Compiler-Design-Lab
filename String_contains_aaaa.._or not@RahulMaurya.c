5ece797f897745cba906ae7509ca066ac5310afc
Rahul maurya <champrahul01@gmail.com>
2021-09-23 10:39:46 +0530

    Update and rename String_contains_aaaa.._or not.c to String_contains_aaaa.._or not@RahulMaurya.c

--- String_contains_aaaa.._or not@RahulMaurya.c
+++ String_contains_aaaa.._or not@RahulMaurya.c
@@ -5,8 +5,8 @@ void main() {
     int i;
     int ans =0,counter=0;
 
-    printf("Name:Sachin Tiwari \n");
-    printf("Roll No.:1901330100236 \n");
+    printf("Name:Rahul Maurya \n");
+    printf("Roll No.:1901330100210 \n");
     printf("Branch/Section: C.S.E. ,5 C \n");
     printf("Enter String : ");
     scanf("%s", &ch);
