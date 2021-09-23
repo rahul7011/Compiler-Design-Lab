52a772dce46275d7154baa7e10649c49c8f7fae6
Rahul maurya <champrahul01@gmail.com>
2021-09-23 10:41:59 +0530

    Update and rename ISCONST.C to ISCONST@RahulMaurya.C

--- ISCONST@RahulMaurya.C
+++ ISCONST@RahulMaurya.C
@@ -6,6 +6,9 @@ void main()
 char ch[100];
 int i=0,flag=0;
 clrscr();
+printf("Name:Rahul Maurya \n");
+printf("Roll No.:1901330100210 \n");
+printf("Branch/Section: C.S.E. ,5 C \n");
 printf("Enter-");
 scanf("%s",&ch);
 while(ch[i]!='\0')
